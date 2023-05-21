d005c1918053c82ea473a517f9a53e216f2f66d5
KxllSwxtch <dimashin9@gmail.com>
2023-05-21 14:16:31 +0900

    updated AboutMe component

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -5,33 +5,39 @@ const AboutMe: React.FC<{}> = (): JSX.Element => {
 	return (
 		<Row className='p-4'>
 			<Col>
-				Здравствуйте,
+				<b>Меня зовут Дмитрий</b>, я Front-End разработчик с 3-мя годами
+				коммерческого опыта. Работаю с технологиями: 1. React.js (Next.js,
+				Remix.js) 2. MongoDB 3. Python (Django, Flask, RestAPI) 4.
+				Figma/Photoshop 5. Vanilla JavaScript 6. HTML/CSS (Tailwind, SASS,
+				Material UI, Less) 7. Supabase 8. Webpack
 				<br />
 				<br />
-				Меня зовут Дмитрий, я Front-End разработчик с 3-мя годами коммерческого
-				опыта. Работаю с технологиями: 1. React.js (Next.js, Remix.js) 2.
-				MongoDB 3. Python (Django, Flask, RestAPI) 4. Figma/Photoshop 5. Vanilla
-				JavaScript 6. HTML/CSS (Tailwind, SASS, Material UI, Less) 7. Supabase
-				8. Webpack
+				<b>Английский на уровне продвинутого (образование получил в Канаде)</b>
 				<br />
 				<br />
-				Английский на уровне продвинутого (образование получил в Канаде)
-				<br />
-				<br />
-				Кратко о своём опыте: Работал в-основном на фрилансе в Канаде. Так же
-				есть коммерческий опыт в канадских компаниях.
+				<i>
+					Кратко о своём опыте: Работал в-основном на фрилансе в Канаде.{' '}
+					<b>Так же есть коммерческий опыт в канадских компаниях.</b>
+				</i>
 				<br />
 				<br />
 				Проект в котором я разработал Landing page и так же учавствовал в
-				разработке кода для обработки запросов blockchain:
-				https://www.walletcs.com/
+				разработке кода для обработки запросов blockchain:{' '}
+				<a href='https://www.walletcs.com/' target='__blank'>
+					Ссылка
+				</a>
 				<br />
 				<br />
-				Проект в котором я принял участие в разработке RSS генератора:
-				https://rss.app/
+				Проект в котором я принял участие в разработке RSS генератора:{' '}
+				<a href='https://rss.app/' target='__blank'>
+					Ссылка
+				</a>
 				<br />
 				<br />
-				Мой Github: https://github.com/KxllSwxtch
+				Мой Github:{' '}
+				<a href='https://github.com/KxllSwxtch' target='__blank'>
+					Ссылка
+				</a>
 			</Col>
 		</Row>
 	)
